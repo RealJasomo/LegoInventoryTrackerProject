7fd400c98d7641126146e1b5cca75fb0a75c8879
realjasomo <jason.cramer6@gmail.com>
2021-01-15 03:05:23 -0500

    updated scripts

--- SQLScripts/create_builds_table.sql
+++ SQLScripts/create_builds_table.sql
@@ -1,12 +1,12 @@
 use LegoInventoryTracker
 go
 
-
 CREATE TABLE Builds
 (
 	Model int,
 	Username dbo.Username,
 	Quantity int,
+	PRIMARY KEY (Model, Username),
 	FOREIGN KEY (Model) references [LegoModel](ID),
 	FOREIGN KEY (Username) references [User](Username)
 )
--- SQLScripts/create_modelcontains_table.sql
+++ SQLScripts/create_modelcontains_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE [ModelContains]
 	Model int,
 	LegoBrick dbo.BrickID,
 	Quantity int,
+	PRIMARY KEY (Model, LegoBrick, Quantity),
 	FOREIGN KEY (Model) references [LegoModel](ID),
 	FOREIGN KEY (LegoBrick) references [LegoBrick](ID)
 )
