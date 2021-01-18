faa37a22786fc26ae6f6cb447d4ac134aca64154
realjasomo <jason.cramer6@gmail.com>
2021-01-18 03:16:35 -0500

    Added navbar for root + add material-ui to project

--- website/lego-inventory-tracker/src/components/Homepage.js
+++ website/lego-inventory-tracker/src/components/Homepage.js
@@ -1,11 +1,23 @@
 import styles from '../css/Homepage.module.css'
 import React, { Component } from 'react'
+import {AppBar, Toolbar,  IconButton, Typography, Button} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 export default class Homepage extends Component {
     render() {
         return (
-            <div>
-                <h1>Lego Inventory Tracker</h1>
+            <div id="nav" className={styles.flexGrow} >
+                <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={styles.flexGrow}>
+                    Lego Inventory Tracker
+                    </Typography>
+                    <Button color="inherit">Login / Sign Up</Button>
+                </Toolbar>
+                </AppBar>
             </div>
         )
     }
