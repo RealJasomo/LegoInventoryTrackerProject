222db5e6fdc45046c4b743ff1b00b19af1d58610
realjasomo <jason.cramer6@gmail.com>
2021-01-19 16:13:53 -0500

    Added link draw for navigator

--- website/lego-inventory-tracker/src/App.js
+++ website/lego-inventory-tracker/src/App.js
@@ -1,33 +1,66 @@
 import styles from "./css/App.module.css"
+import { ReactComponent as BrickLogo } from "./res/brick.svg";
 import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 import {Homepage, BrickCollection, ModelCollection, SetCollection} from './components'
-import {AppBar, Toolbar,  IconButton, Typography, Button} from '@material-ui/core'
+import {AppBar, Toolbar,  IconButton, Typography, 
+        Button, Drawer, List, ListItem, ListItemIcon, ListItemText, SvgIcon} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 
 export default class App extends Component {
+  constructor(){
+  super();
+   this.state = {
+    menuOpen: false
+  }
+}
+  toggleDrawer = (isOpen) => () =>{
+    this.setState({
+      menuOpen: isOpen
+    })
+  }
   render() {
     return (
       <>
+      <Router>
         <div id="nav" className={styles.flexGrow} >
                   <AppBar position="static">
                   <Toolbar>
-                      <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu">
+                      <IconButton edge="start" className={styles.menuButton} onClick={this.toggleDrawer(true)}color="inherit" aria-label="menu">
                       <MenuIcon />
                       </IconButton>
-                      <Typography variant="h6" className={styles.flexGrow}>
+                      <Typography variant="h6" className={`${styles.flexGrow} ${styles.link}`} component={Link} to='/'>
                       Lego Inventory Tracker
                       </Typography>
                       <Button color="inherit">Login / Sign Up</Button>
                   </Toolbar>
                   </AppBar>
+                  <Drawer open={this.state.menuOpen} onClose={this.toggleDrawer(false)}>
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      onClick={this.toggleDrawer(false)}
+                      onKeyDown={this.toggleDrawer(false)}
+                    >
+                       <List>
+                        {['Bricks', 'Models', 'Sets'].map((text, index, arr) => (
+                          <Link to={`/${arr[index].toLowerCase()}`} className={styles.link}>
+                            <ListItem button key={text}>
+                              <ListItemIcon><SvgIcon><BrickLogo/></SvgIcon></ListItemIcon>
+                              <ListItemText primary={text} />
+                            </ListItem>
+                          </Link>
+                        ))}
+                      </List>
+                    </div>
+                  </Drawer>
         </div>
         <div id="app-container">
-          <Router>
             <Switch>
               <Route exact path="/">
                 <Homepage />
@@ -48,8 +81,8 @@ export default class App extends Component {
                 <BrickCollection />
               </Route>
             </Switch>
-          </Router>
         </div>
+        </Router>
       </>
     )
   }
