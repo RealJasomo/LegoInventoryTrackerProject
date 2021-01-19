9f24c0e80d55fd0e008499f26914d4880a3a6727
realjasomo <jason.cramer6@gmail.com>
2021-01-19 18:46:10 -0500

    Added temporary homepage

--- website/lego-inventory-tracker/src/components/Homepage.js
+++ website/lego-inventory-tracker/src/components/Homepage.js
@@ -5,7 +5,12 @@ import React, { Component } from 'react'
 export default class Homepage extends Component {
     render() {
         return (
-            <div>Homepage</div>
+            <>
+            <div className={styles.header}>
+                <h1>Welcome to the Lego Inventory Tracker!</h1>
+                <p>Created by: Luke Ferderer, Jamari Morisson, and Jason Cramer</p>
+            </div>
+            </>
         )
     }
 }
