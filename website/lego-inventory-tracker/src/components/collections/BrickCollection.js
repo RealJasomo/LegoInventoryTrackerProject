f3d7b26e436ee70316e8fac6a87807643fbdc605
realjasomo <jason.cramer6@gmail.com>
2021-01-19 18:56:48 -0500

    Adjusted card layout

--- website/lego-inventory-tracker/src/components/collections/BrickCollection.js
+++ website/lego-inventory-tracker/src/components/collections/BrickCollection.js
@@ -7,6 +7,16 @@ export default class BrickCollection extends Component {
             <div className={styles.flex}>
                 <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
                 <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
+                <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
+                <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
+                <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
+                <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
+                <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
+                <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
+                <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
+                <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
+                <BrickCard id="3005" url="https://img.bricklink.com/ItemImage/PN/11/3005.png" color="black" name="1 x 1"/>
+                <BrickCard id="3003" url="https://img.bricklink.com/ItemImage/PN/7/3003.png" color="blue" name="2 x 2"/>
             </div>
         )
     }
